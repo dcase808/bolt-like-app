04c124c11199b66289dcb4ba64bbd1f0a8d778bf
filipo4664 <83177497+filipo4664@users.noreply.github.com>
2022-01-25 13:38:08 +0100

    Update Przejazdy.js

--- comps/Przejazdy.js
+++ comps/Przejazdy.js
@@ -15,23 +15,24 @@ const Przejazdy = () => {
 
     return (
         <NativeBaseProvider>
-                <Tab.Navigator
+                <Tab.Navigator 
           initialRouteName="Zamow"
           screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, color, size }) => {
               size = 28
               if(route.name === 'Zamów')
-                return <Ionicons name={focused ? 'navigate' : 'navigate-outline'} size={size} color="black" />
+                return <Ionicons name={focused ? 'navigate' : 'navigate-outline'} size={size} color={focused ? "white":"black"} />
               else if(route.name === 'Historia')
-                return <Ionicons name={focused ? 'book' : 'book-outline'} size={size} color="black" />
+                return <Ionicons name={focused ? 'book' : 'book-outline'} size={size} color={focused ? "white":"black"} />
               else if(route.name === 'Samochody')
-                return <Ionicons name={focused ? 'car' : 'car-outline'} size={size} color="black" />
-            }
+                return <Ionicons name={focused ? 'car' : 'car-outline'} size={size} color={focused ? "white":"black"} />
+            },
+
           })}
         >
-            <Tab.Screen name="Zamów" component={Zamow}/>
-            <Tab.Screen name="Historia" component={Historia}/>
-            <Tab.Screen name="Samochody" component={Samochody}/>
+            <Tab.Screen  options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="Zamów" component={Zamow}/>
+            <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="Historia" component={Historia}/>
+            <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="Samochody" component={Samochody}/>
         </Tab.Navigator>
         </NativeBaseProvider>
     )
