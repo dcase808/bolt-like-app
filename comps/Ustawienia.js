f15e236dc1f2028b731f337e01c2f2cab620b627
Ziomidlo <kziomek33@gmail.com>
2022-01-25 15:30:28 +0100

    Update Ustawienia.js

--- comps/Ustawienia.js
+++ comps/Ustawienia.js
@@ -10,28 +10,31 @@ import Konto from './Konto'
 const Tab = createBottomTabNavigator()
 
 
-const Ustawienia = (props) => {
+const Ustawienia = (prop) => {
+
+  const KontoWithProps = props => (
+    <Konto setIsLogged={prop.setIsLogged} login={prop.login} />
+  );
 
     return (
-        <NativeBaseProvider>
-                <Tab.Navigator
-          initialRouteName="Konto"
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              size = 28
-              if(route.name === 'Konto')
-                return <Ionicons name={focused ? 'person' : 'person-outline'} size={size} color="black" />
-              else if(route.name === 'O nas')
-                return <Ionicons name={focused ? 'alert-circle' : 'alert-circle-outline'} size={size} color="black" />
-                
-            }
-          })}
+      <NativeBaseProvider>
+      <Tab.Navigator
+        initialRouteName="Konto"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            size = 28
+            if(route.name === 'Konto')
+              return <Ionicons name={focused ? 'person' : 'person-outline'} size={size} color="black" />
+            else if(route.name === 'O nas')
+              return <Ionicons name={focused ? 'alert-circle' : 'alert-circle-outline'} size={size} color="black" />
+              
+          }
+        })}
         >
-            <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="Konto" component={() => <Konto setIsLogged={props.setIsLogged} login={props.login} /> } />
-            <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="O nas" component={ONas}/>
-            
-        </Tab.Navigator>
-        </NativeBaseProvider>
+        <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="Konto" component={KontoWithProps}  />
+        <Tab.Screen options={{ tabBarActiveTintColor: 'white', tabBarActiveBackgroundColor: '#3489eb',tabBarInactiveTintColor: 'black'}} name="O nas" component={ONas}/>
+      </Tab.Navigator>
+      </NativeBaseProvider>
     )
 }
 
